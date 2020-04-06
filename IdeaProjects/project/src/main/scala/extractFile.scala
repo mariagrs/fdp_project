70c38f7cc082e9b16639856bbebae79a3a553ea9
unknown <maria.graus1998@gmail.com>
2020-04-06 14:16:01 +0200

    second commit

--- IdeaProjects/project/src/main/scala/extractFile.scala
+++ IdeaProjects/project/src/main/scala/extractFile.scala
@@ -5,6 +5,7 @@ import scala.io.{BufferedSource, Source}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object extractFile {
+
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir", "C:\\winutil\\")
     val conf = new SparkConf()
@@ -14,13 +15,31 @@ object extractFile {
     val sc = new SparkContext(conf) // Create Spark Context
     // Load local file data
     val spark : SparkSession = SparkSession.builder
-      .appName("test")
+      .appName("loadcsv")
       .master("local[2]")
       .getOrCreate()
 
-    val emp_data = spark.read.csv("C:\\Users\\test\\parking_violations.csv")
-    val df1 = emp_data.select("_c1", "_c5", "_c23", "_c24","_c39").show()
 
+    val sourceDf = spark.read.format("csv").option("header", "true").load("C:\\Users\\test\\parking_violations.csv")
+    val df1 = sourceDf.select("Plate ID", "Violation description", "House Number","Street Name").toDF()
+
+    val randomRow = df1.sample(false,0.1).toDF()
+
+    randomRow.show(1)
+
+    val plate_id = randomRow.first().getString(0)
+    val violation_description = randomRow.first().getString(1)
+    val house_number = randomRow.first().getString(2)
+    val street_name = randomRow.first().getString(3)
+
+    if (plate_id.equals(null) || violation_description.equals(null)){
+      print("alert")
+    }
+    else{
+      print("Plate Id : "+ plate_id + " / Violation description : "+ violation_description + " / House number : "+ house_number + " / Street name : "+ street_name )
+    }
+
+    //df1.where(df1.col("Plate ID").isNull).show()
   }
 
 }
